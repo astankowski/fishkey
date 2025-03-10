4091d13dd7253e3766ccc29400b41aee3568844f
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-03-10 13:20:18 +0100

    chore: remove server proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,15 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://3t2d1gmx79.execute-api.eu-north-1.amazonaws.com/dev/',
-        changeOrigin: true,
-        secure: false
-      }
     }
   }
 })
